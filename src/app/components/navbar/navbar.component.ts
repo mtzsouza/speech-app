f6c5d041b03f9441ee1e4fe564b6036d251c7fdd
Matheus Souza <88462129+mtzsouza@users.noreply.github.com>
2025-02-24 12:16:32 -0600

    added translation function for navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { PreferencesComponent } from '../preferences/preferences.component';
 import { Router } from '@angular/router';
+import { LanguageService } from '../../services/language.service';
+import * as english from '../../utils/english.json'
 
 @Component({
   selector: 'app-navbar',
@@ -13,6 +15,13 @@ import { Router } from '@angular/router';
 export class NavbarComponent {
   auth = inject(AuthService);
   router = inject(Router);
+  languageService = inject(LanguageService);
+
+  userLanguage = english;
+
+  async ngOnInit(): Promise<void> {
+    this.userLanguage = await this.languageService.getLanguage();
+  }
 
   currentPage = this.router.url;
 }
